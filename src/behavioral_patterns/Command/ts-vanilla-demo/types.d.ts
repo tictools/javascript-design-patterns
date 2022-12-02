07fa7b96d90bf945bab3e176cf27911cc9f1ead9
tictools <jaumevillarreal@gmail.com>
2022-12-02 19:35:38 +0100

    Define HistoryCommands interface

--- src/behavioral_patterns/Command/ts-vanilla-demo/types.d.ts
+++ src/behavioral_patterns/Command/ts-vanilla-demo/types.d.ts
@@ -2,3 +2,9 @@ export interface CommandController {
   execute: (value: number) => number;
   undo: (value: number) => number;
 }
+
+export interface HistoryCommands {
+  push: (command: CommandController) => void;
+  pop: () => void;
+  list: () => CommandController[];
+}
