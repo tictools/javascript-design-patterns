190946752322157358e927daf8e8f732aee258d8
tictools <jaumevillarreal@gmail.com>
2022-12-04 19:05:38 +0100

    Add new Calculator test case to spy on pop method

--- src/behavioral_patterns/Command/ts-vanilla-demo/__tests__/Calculator/Calculator.test.ts
+++ src/behavioral_patterns/Command/ts-vanilla-demo/__tests__/Calculator/Calculator.test.ts
@@ -6,12 +6,12 @@ import { CommandOperations } from "../../types";
 describe("given class Calculator", () => {
   let spyOnExecute: jest.SpyInstance<number, [value: number]>;
   let spyOnPushHistory: jest.SpyInstance<void, [command: CommandOperations]>;
-  // let spyOnPopHistory: jest.SpyInstance<MockCommand, []>;
+  let spyOnPopHistory: jest.SpyInstance<MockCommand, []>;
 
   beforeEach(() => {
     spyOnExecute = jest.spyOn(MockCommand.prototype, "execute");
     spyOnPushHistory = jest.spyOn(MockhistoryCommand.prototype, "push");
-    // spyOnPushHistory = jest.spyOn(MockhistoryCommand.prototype, "pop");
+    spyOnPopHistory = jest.spyOn(MockhistoryCommand.prototype, "pop");
   });
 
   afterEach(() => {
@@ -37,5 +37,13 @@ describe("given class Calculator", () => {
     calculator.executeCommand(new MockCommand(0));
     expect(spyOnExecute).toHaveBeenCalledTimes(1);
     expect(spyOnPushHistory).toHaveBeenCalledTimes(1);
+  });
+
+  test("when undo method is invoked then methods are called as expected", () => {
+    const mockedHistory = new MockhistoryCommand();
+    const calculator = new Calculator(mockedHistory);
+
+    calculator.undo();
+    expect(spyOnPopHistory).toHaveBeenCalledTimes(1);
   });
 });
