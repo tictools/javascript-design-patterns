7dd887a896fd45df63ffcf98f62525eea4db7882
tictools <jaumevillarreal@gmail.com>
2022-12-04 19:54:24 +0100

    Define index example

--- src/behavioral_patterns/Command/ts-vanilla-demo/index.ts
+++ src/behavioral_patterns/Command/ts-vanilla-demo/index.ts
@@ -1,5 +1,37 @@
 import Calculator from "./Calculator/Calculator";
+import AddCommand from "./Command/AddCommand";
+import SubstractCommand from "./Command/SubstractCommand";
 import CommandHistory from "./CommandHistory/CommandHistory";
+
+let result;
 
 const history = new CommandHistory();
 const calculator = new Calculator(history);
+
+calculator.executeCommand(new AddCommand(10));
+calculator.executeCommand(new AddCommand(5));
+calculator.executeCommand(new AddCommand(3));
+calculator.executeCommand(new AddCommand(2));
+calculator.executeCommand(new SubstractCommand(5));
+result = calculator.total();
+console.log({ result });
+
+calculator.undo();
+result = calculator.total();
+console.log({ result });
+
+calculator.undo();
+result = calculator.total();
+console.log({ result });
+
+calculator.undo();
+result = calculator.total();
+console.log({ result });
+
+calculator.undo();
+result = calculator.total();
+console.log({ result });
+
+calculator.undo();
+result = calculator.total();
+console.log({ result });
