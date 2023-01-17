5846ceea99e4fb2fdf41aebcda15c4b902c265a1
tictools <jaumevillarreal@gmail.com>
2023-01-17 17:58:43 +0100

    Define index as Client

--- src/structural_patterns/composite/client.ts
+++ src/structural_patterns/composite/client.ts
@@ -1,25 +1,26 @@
-import Compound from "./Compound/Compound";
-import Resource from "./Resource/Resource";
+import Link from "./Link/Link";
+import Podcast from "./Podcast/Podcast";
+import Post from "./Post/Post";
+import Video from "./Video/Video";
 
 /**
  * Create podcast compound
  * Create podcast resources
  * Add resources to compound
  */
-const podcastCompound = new Compound("Podcast Compound");
-podcastCompound.addResource(new Resource("POST"));
-podcastCompound.addResource(new Resource("LINK"));
-podcastCompound.addResource(new Resource("LINK"));
+const podcast = new Podcast("Podcast NodeCourse");
+podcast.appendChild(new Video("VIDEO FOR PODCAST"));
+podcast.appendChild(new Link("LINK FOR PODCAST"));
 
 /**
  * Create video compound
  * Create video resources
  * Add resources to compound
  */
-const videoCompound = new Compound("Video Compound");
-videoCompound.addResource(new Resource("POST"));
-videoCompound.addResource(new Resource("LINK"));
-videoCompound.addResource(new Resource("PODCAST"));
-videoCompound.addResource(podcastCompound);
+const post = new Post("Post NodeCourse");
+post.appendChild(new Video("VIDEO FOR POST"));
+post.appendChild(new Link("LINK FOR POST"));
+podcast.appendChild(post);
 
-videoCompound.execute();
+console.log("🚀 ~ ", { podcastElements: podcast.listChildren() });
+podcast.execute();
