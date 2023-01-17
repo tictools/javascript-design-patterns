c034fa1292d6333c11ec123d211af61c15d83107
tictools <jaumevillarreal@gmail.com>
2023-01-17 14:04:18 +0100

    Redefine ElementCourse as abstract class. Adapt tests

--- src/structural_patterns/composite/__tests__/ElementCourse/ElementCourse.test.ts
+++ src/structural_patterns/composite/__tests__/ElementCourse/ElementCourse.test.ts
@@ -1,12 +1,12 @@
-import MockResource from "../../mocks/MockResource";
-import { ResourceOperations } from "../../types";
+import MockElementCourse from "../../mocks/MockElementCourse";
+import { ElementOperations } from "../../types";
 
 describe("given a class Resource", () => {
   describe("when an instance is created", () => {
-    let resource: ResourceOperations;
+    let resource: ElementOperations;
 
     beforeEach(() => {
-      resource = new MockResource("resource");
+      resource = new MockElementCourse("resource");
     });
 
     test("then method 'getId()' should be defined", () => {
@@ -23,15 +23,16 @@ describe("given a class Resource", () => {
   });
 
   describe("when method", () => {
-    let resource = new MockResource("POST");
+    let resource: ElementOperations;
 
     beforeEach(() => {
-      resource = new MockResource("RESOURCE");
+      resource = new MockElementCourse("RESOURCE");
     });
 
-    test("'getName()' is called then expected name should be returned", () => {
+    test("'getId()' is called then expected id should be returned", () => {
       const id = resource.getId();
       expect(typeof id).toBe("number");
+      expect(id).toEqual(123);
     });
 
     test("'getName()' is called then expected name should be returned", () => {
@@ -42,7 +43,9 @@ describe("given a class Resource", () => {
     test("'execute()' is called then expected array should be returned", () => {
       const spyOnLog = jest.spyOn(globalThis.console, "log");
       resource.execute();
-      expect(spyOnLog).toHaveBeenCalledWith("Executing from Resource...");
+      expect(spyOnLog).toHaveBeenCalledWith(
+        "Executing from mocked Resource..."
+      );
     });
   });
 });
