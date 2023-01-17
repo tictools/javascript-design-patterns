b7469ad3c1c53eb13cf9144c31602fc2ab9c3d35
tictools <jaumevillarreal@gmail.com>
2023-01-17 14:11:30 +0100

    Format code + remove comment

--- src/structural_patterns/composite/ElementCourse/ElementCourse.ts
+++ src/structural_patterns/composite/ElementCourse/ElementCourse.ts
@@ -8,41 +8,16 @@ export default abstract class ElementCourse implements ElementOperations {
     this.name = name;
     this.id = Math.floor(Date.now() * Math.random());
   }
+
   getId() {
     return this.id;
   }
+
   getName() {
     return this.name;
   }
+
   execute() {
     console.log(`Method execute() invoked from element ${this.getName()}`);
   }
 }
-
-// import { ElementOperations } from "../types";
-
-// export default class Resource implements ElementOperations {
-//   private name: string;
-//   private id: number;
-
-//   constructor(name: string) {
-//     this.id = Math.floor(Date.now() * Math.random());
-//     this.name = name;
-//   }
-
-//   getId(): number {
-//     return this.id;
-//   }
-
-//   getName(): string {
-//     return this.name;
-//   }
-
-//   execute() {
-//     console.log(
-//       `Executing from an instance of ::${
-//         this.constructor.name
-//       }:: with name ${this.getName()}`
-//     );
-//   }
-// }
